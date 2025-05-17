834abbae34057c9cfca86b8459ee9bc39ba208aa
danillevko <danillevko1212@gmail.com>
2025-05-17 10:08:59 -0600

    done

--- script.js
+++ script.js
@@ -7,3 +7,24 @@
             document.querySelector('.video-thumbnail').style.display = 'none';
             document.querySelector('.play-button').style.display = 'none';
         }
+
+        function toggleContent() {
+   const buttons = document.querySelectorAll('.process-link-mobile');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const container = button.closest('.show-more-container'); 
+    const content = container.querySelector('.more-text');
+    const mainText = container.querySelector('.process-span-mobile');
+
+    content.classList.toggle('show');
+    mainText.classList.toggle('hidden');
+
+    if (content.classList.contains('show')) {
+      button.textContent = 'Show less';
+    } else {
+      button.textContent = 'Show more';
+    }
+  });
+});
+        }
